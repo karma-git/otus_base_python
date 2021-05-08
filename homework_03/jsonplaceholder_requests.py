372c52826fce99fae507bd276b37d7342e464949
Andrei Horbach <andrewhorbach@gmail.com>
2021-05-08 22:43:20 +0300

    processing with hw3

--- homework_03/jsonplaceholder_requests.py
+++ homework_03/jsonplaceholder_requests.py
@@ -1,6 +1,58 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
+---
+Забираем данные из API и превращаем в dataclass, возвращаем два списка - юзеры и посты
 """
+import asyncio
+from aiohttp import ClientSession
+from dataclasses import dataclass
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+@dataclass
+class User:
+    name: str
+    username: str
+    email: str
+
+
+@dataclass
+class Post:
+    title: str
+    body: str
+
+
+async def get_json(url: str) -> dict:
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+
+async def receive_api_data() -> tuple:
+    get_users = asyncio.create_task(get_json(url=USERS_DATA_URL))
+    get_posts = asyncio.create_task(get_json(url=POSTS_DATA_URL))
+    raw_users = await get_users
+    raw_posts = await get_posts
+
+    users = [User(name=user['name'],
+                  username=user['username'],
+                  email=user['email'])
+             for user in raw_users]
+
+    posts = [Post(title=post['title'],
+                  body=post['body'])
+             for post in raw_posts]
+
+    return users, posts
+
+
+# TODO: import that to other module, delete debug
+def main():
+    result = asyncio.run(receive_api_data())
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- homework_03/models.py
+++ homework_03/models.py
@@ -9,8 +9,79 @@
 """
 
 import os
+import asyncio
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    Text
+)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-PG_CONN_URI = os.environ.get("PG_CONN_URI") or "postgresql://postgres:password@localhost/postgres"
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
-Base = None
-Session = None
+engine = create_async_engine(PG_CONN_URI, echo=True)
+
+Base = declarative_base()
+
+Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+
+    post = relationship("Post", back_populates="user")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name}, " \
+               f"username={self.username}, email={self.email})"
+
+    def __repr__(self):
+        return str(self)
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    body = Column(Text)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User", back_populates="post")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title}, " \
+               f"user_id={self.user_id})"
+
+    def __repr__(self):
+        return str(self)
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def main():
+    print("Starting main")
+    await create_tables()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
+
+
+
+
+
+

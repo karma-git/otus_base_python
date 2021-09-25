b100656ffb5e51d2428dd955dea67bd1eb116783
Andrew Horbach <andrewhorbach@gmail.com>
2021-09-25 15:13:27 +0300

    chore: good query

--- practise/django/2.django-query-debugtoolbar/web/blog/views.py
+++ practise/django/2.django-query-debugtoolbar/web/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog.models import Author
 
 # Create your views here.
 def root(request):
-    return render(request, 'blog/index.html', {'ctx': 'hello world'})
+    authors = Author.objects.all()
+    return render(request, 'blog/index.html', {'authors': authors})

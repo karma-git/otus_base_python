7502199e427eb3ef94f0473b4cceb206f29ef42f
Andrew Horbach <andrewhorbach@gmail.com>
2021-09-25 07:57:12 +0300

    chore: add app and models with migrations

---
+++ practise/django/2.django-query-debugtoolbar/web/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'Name={self.name},email={self.email}'
+
+class Articles(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
+
+    def __str__(self):
+        return f'Title={self.title},author={self.author}'
+
+class Tags(models.Model):
+    name = models.CharField(max_length=64)
+    articles = models.ManyToManyField(Articles)
+
+    def __str__(self):
+        return f'Name={self.name}'

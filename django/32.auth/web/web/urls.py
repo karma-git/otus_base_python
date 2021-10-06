8074f0efac6b4c5593fe62ee609338a4b65f9204
a.horbach <a.horbach@studionx.ru>
2021-10-06 09:56:47 +0300

    chore: add index page, check username

--- django/32.auth/web/web/urls.py
+++ django/32.auth/web/web/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from web.settings import DEBUG
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='blog/index.html'), name="index"),
     path('admin/', admin.site.urls),
 ]
 

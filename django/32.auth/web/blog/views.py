d3fe5673037cb2d97415f115f78954aa05376a57
a.horbach <a.horbach@studionx.ru>
2021-10-06 10:36:34 +0300

    chore: registartion form

--- django/32.auth/web/blog/views.py
+++ django/32.auth/web/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView, CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from .models import User
 
 # Create your views here.
+
+class Registration(CreateView):
+     model = User
+     success_url = reverse_lazy("login")
+     form_class = CustomUserCreationForm
--- django/32.auth/web/web/urls.py
+++ django/32.auth/web/web/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+import blog.views as blog
 from web.settings import DEBUG
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='blog/index.html'), name="index"),
+    path('registration', blog.Registration.as_view(), name="registration"),
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
 ]

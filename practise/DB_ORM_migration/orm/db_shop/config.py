15c4c326b45c03faeef1d7047806b8b492466c7c
a.horbach <a.horbach@studionx.ru>
2021-10-10 12:02:58 +0300

    refactor: branch PythonBasic.2021-05 current state

--- practise/DB_ORM_migration/orm/db_shop/config.py
+++
@@ -1,3 +0,0 @@
-from os import environ as env
-DATABASE_URI = f"postgresql://{env['user']}:{env['password'] }@{env['host']}:{env['port']}/{env['database']}"  # sqlAlchemy 1.4 - postgresql as driver
-

6d65f904479683fd0b5d03b40b707d079729c4ca
Andrew Horbach <andrewhorbach@gmail.com>
2021-10-12 23:51:06 +0300

    chore: login required tests

--- django/33.tests/web/blog/tests/test_auth_elements.py
+++ django/33.tests/web/blog/tests/test_auth_elements.py
@@ -5,7 +5,7 @@ from blog.models import User
 import random
 import string
 
-class TestIndexPage(TestCase):
+class TestAuthElements(TestCase):
     """
     Запросы к элементам REGISTER / LOGIN / LOGOUT
     """

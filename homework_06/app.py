0cb0e41368cdff1db71e21162331a2814e78438c
Andrew Horbach <andrewhorbach@gmail.com>
2021-10-14 14:55:39 +0300

    chore: add db init

--- homework_06/app.py
+++ homework_06/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from views.products import products_bp
+from models.database import db
 
 app = Flask(__name__)
 app.register_blueprint(products_bp, url_prefix="/products")
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///./products.db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+db.init_app(app)
 
 @app.route("/")
 def index():

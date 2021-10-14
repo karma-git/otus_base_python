a32ae98f9065fbfbb81fa2e12ff4c320dd39e3e7
Andrew Horbach <andrewhorbach@gmail.com>
2021-10-14 13:01:21 +0300

    chore: add blueprint

--- homework_06/app.py
+++ homework_06/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from views.products import products_bp
 
 app = Flask(__name__)
-
+app.register_blueprint(products_bp, url_prefix="/products")
 
 @app.route("/")
 def index():
